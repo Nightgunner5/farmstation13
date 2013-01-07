b0ad245a494a1e822fded47d6eba13e26014ff48
Ben L <nightgunner5@llamaslayers.net>
2013-01-07 13:34:20 -0600

    tick

--- planter.go
+++ planter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"math/rand"
 )
 
@@ -18,12 +19,12 @@ type Solution struct {
 type Planter struct {
 	*Crop
 	Solution
-	Dehydration  float32 // if this reaches 100, the plant dies.
+	Dehydration  float32 // if this reaches +-100, the plant dies. Negative values are drowning.
 	Health       float32 // if this is 0, the plant is dead.
 	YieldScale   float32 // multiplier for yield.
 	TimeScale    float32 // multiplier for time (higher is slower).
-	GrowthCycle  uint16
-	HarvestsLeft uint16 // ten times the estimated number of remaining harvests.
+	GrowthCycle  uint16  // if this is 0, the plant is harvestable.
+	HarvestsLeft uint16  // ten times the estimated number of remaining harvests.
 
 	// TODO: mutation
 }
@@ -33,12 +34,98 @@ func (p *Planter) Defaults() {
 	p.Health = 100
 	p.YieldScale = 1
 	p.TimeScale = 1
-	p.GrowthCycle = 0
-	p.HarvestsLeft = 0
+}
+
+func moveTowards(current *float32, goal, scale float32) {
+	if *current > goal {
+		*current -= scale * rand.Float32()
+		if *current < goal {
+			*current = goal
+		}
+	} else {
+		*current += scale * rand.Float32()
+		if *current > goal {
+			*current = goal
+		}
+	}
 }
 
 func (p *Planter) Tick() {
-	// TODO
+	if p.GrowthCycle != 0 {
+		growth := rand.Intn(int(10 / p.TimeScale))
+		if p.GrowthCycle < growth {
+			p.GrowthCycle = 0
+		} else {
+			p.GrowthCycle -= growth
+		}
+	}
+
+	if p.Health <= 0 {
+		return
+	}
+
+	moveTowards(&p.Solution.Water, 0, 5)
+	moveTowards(&p.Solution.Compost, 0, 5)
+	moveTowards(&p.Solution.ToxicSlurry, 0, 5)
+	moveTowards(&p.Solution.Mutriant, 0, 5)
+	moveTowards(&p.Solution.GroBoost, 0, 5)
+	moveTowards(&p.Solution.TopCrop, 0, 5)
+	moveTowards(&p.TimeScale, 1, 0.01)
+	moveTowards(&p.YieldScale, 1, 0.01)
+
+	if p.Solution.Water > 200 {
+		moveTowards(&p.Dehydration, -100, 10)
+	} else if p.Solution.Water <= 0 {
+		moveTowards(&p.Dehydration, 100, 10)
+	} else {
+		moveTowards(&p.Dehydration, 0, 10)
+	}
+
+	if p.Solution.Compost > 0 {
+		moveTowards(&p.Health, 100, 5)
+		moveTowards(&p.TimeScale, 0, 0.01)
+		if p.YieldScale < 10 { // don't move down if there's TopCrop
+			moveTowards(&p.YieldScale, 10, 0.05)
+		}
+	}
+
+	if p.Solution.ToxicSlurry > 0 {
+		moveTowards(&p.Health, 0, 10)
+		moveTowards(&p.Dehydration, 100, 10)
+		moveTowards(&p.YieldScale, 0, 0.05)
+		// TODO: mutation
+	}
+
+	if p.Solution.Mutriant > 0 || p.Solution.GroBoost > 0 || p.Solution.TopCrop > 0 {
+		moveTowards(&p.Health, 100, 1)
+
+		if p.Solution.Mutriant > 0 {
+			moveTowards(&p.Health, 0, 10)
+			// TODO: mutation
+		}
+
+		if p.Solution.GroBoost > 0 {
+			moveTowards(&p.TimeScale, 0.001, 0.01)
+			moveTowards(&p.Solution.Water, 0, 10) // extra gulp
+		}
+
+		if p.Solution.TopCrop > 0 {
+			moveTowards(&p.YieldScale, 20, 0.05)
+			if rand.Intn(100) == 0 && p.HarvestsLeft != 0 {
+				p.HarvestsLeft--
+			}
+		}
+	}
+
+	if p.Dehydration < -50 || p.Dehydration > 50 {
+		moveTowards(&p.Health, 0)
+	}
+	if p.Dehydration <= -100 || p.Dehydration >= 100 {
+		p.Health = 0
+	}
+	if p.Health > 100 {
+		p.Health = 100
+	}
 }
 
 func (p *Planter) Harvest() int {
@@ -46,13 +133,13 @@ func (p *Planter) Harvest() int {
 		return -3
 	}
 
-	if p.GrowthCycle != 0 || p.Crop.Yield == 0 {
+	if p.Health <= 0 && (p.GrowthCycle != 0 || p.HarvestsLeft == 0 || p.Crop.Yield == 0) {
+		p.Crop = nil
+		p.Defaults()
 		return -2
 	}
 
-	if p.Health <= 0 {
-		p.Crop = nil
-		p.Defaults()
+	if p.GrowthCycle != 0 || p.Crop.Yield == 0 {
 		return -1
 	}
 
@@ -66,6 +153,6 @@ func (p *Planter) Harvest() int {
 	}
 
 	p.HarvestsLeft -= harvestsUsed
-	p.GrowthCycle = uint16(float32(p.Crop.Time) * p.TimeScale)
+	p.GrowthCycle = uint16(p.Crop.Time)
 	return yield
 }
